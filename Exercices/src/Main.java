87c0ceaa886e076070a81b6f7e3f1b57fb381af8
zaqueurodrigues <zakeurodrigues@outlook.com>
2021-12-29 22:11:11 -0300

    implementation sum with Scanner

--- Exercices/src/Main.java
+++ Exercices/src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Project Created");
-		System.out.println("sum: " + (2 + 2));
+		
+		
+	//	System.out.println("Project Created");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numberOne = sc.nextInt();
+		int numberTwo = sc.nextInt();
+		
+		System.out.println("sum: " + (numberOne + numberTwo));
+		
+		sc.close();
 
 	}
 
